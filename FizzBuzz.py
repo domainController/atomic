5b69d7acb07185b499e92b413e2e856b4b94379c
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-06 16:47:14 +0000

    simpliste,torche en qques minutes

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+for num in range(1, 101):
+    if num % 3 is 0:
+        print("Fizz")
+    elif num % 5 is 0:
+        print("Buzz")
+    elif num % 15 is 0:
+        print("FizzBuzz")
+    else:
+        print(num)
+
+        
+
