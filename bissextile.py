3bf7ae8cd443cef02c21227a5381a2d0ee31b0d1
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-17 17:04:00 +0000

    deep dive into leap algo

--- bissextile.py
+++ bissextile.py
@@ -1,9 +1,6 @@
 annee=0
 annee = int(annee)
-annee=input("Saisissez une annee: ")
-if annee % 4 != 0:
-    print("Cette annee n'est pas une annee bissextille")
-elif annee % 4 == 0 and annee % 100 == 0:
-    print("Cette annee est bien une anne bissextile")
-else:
-    print("Cette annee n'est pas une annee bissextille")
+annee = input("Saisissez une annee: ")
+if annee % 4 != 0: print("Cette annee n'est pas une annee bissextile"); 
+elif annee % 4 == 0 and annee % 100 == 0: print("Cette annee est bien une annee bissextile")
+else: print("Cette annee n'est pas une annee bissextile")
