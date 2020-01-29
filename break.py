bc639676fc313d2fcaa05a37a4a82e0aa1cc7b73
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-29 19:39:06 +0000

    cannot run functions in repl

---
+++ break.py
@@ -0,0 +1,6 @@
+while 1:
+    lettre = input("Tapez 'Q' pour quitter : ")
+    if lettre == "Q":
+        print("Fin de la boucle")
+        break 
+    /* ne fonctionne pas, ne comprends pas tres bien le principe */
---
+++ fx7.py
@@ -0,0 +1,7 @@
+def fx7():
+    nb = 7
+    i = 0
+    while i < 10 :
+        print( i +1, "*", nb, "=", ( i+1) * nb)
+        i += 1
+
