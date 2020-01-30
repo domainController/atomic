1975f436ecbfd2818372f634a981efaf06c1ce3a
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-30 18:12:03 +0000

    cannot import module

---
+++ multipli.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.7
+
+def table(nb, max=10):
+    i = 0
+while i < max:
+     print(i + 1,"*",nb,"=",(i+1) * nb)
+     i += 1
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+
+import os
+from multipli import *
+
+# test de la fonction table
+table(3,20)
+os.system("pause")
